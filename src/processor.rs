1de11ec19160e3fab4316c68921f17aa619f4ca9
Jay Kickliter <jay@kickliter.com>
2022-05-03 10:15:00 -0500

    initial commit

--- src/processor.rs
+++ src/processor.rs
@@ -9,8 +9,9 @@ use solana_program::{
 
 /// Define the type of state stored in accounts
 #[derive(BorshSerialize, BorshDeserialize, Debug)]
-pub struct AccountString {
-    pub memo: [u8; 6],
+pub struct AuthCounter {
+    pub authority: Pubkey,
+    pub count: u32,
 }
 
 /// Instruction processor
@@ -21,21 +22,40 @@ pub fn process_instruction(
 ) -> ProgramResult {
     let accounts_iter = &mut accounts.iter();
     let account = next_account_info(accounts_iter)?;
+    let authority = next_account_info(accounts_iter)?;
+
+    assert_eq!(std::mem::size_of::<AuthCounter>(), 36);
+
+    if !authority.is_signer {
+        msg!("Authority is not a signer");
+        return Err(ProgramError::MissingRequiredSignature);
+    }
 
     if account.owner != program_id {
         msg!("this program does not own account {:?}", account);
         return Err(ProgramError::IncorrectProgramId);
     }
 
-    let acct_memo = AccountString {
-        memo: {
-            let mut acct_str = [0; 6];
-            acct_str.copy_from_slice(&instruction_data[..6]);
-            acct_str
-        },
+    let mut ctr = AuthCounter::try_from_slice(&account.data.borrow())?;
+
+    if ctr.count == 0 {
+        ctr.authority = *authority.key
+    } else if &ctr.authority != authority.key {
+        msg!(
+            "counter authority {:?} does not match provided authority {:?}",
+            ctr.authority,
+            account.key
+        );
+        return Err(ProgramError::AccountAlreadyInitialized);
+    }
+
+    match instruction_data {
+        &[] => ctr.count += 1,
+        &[b0, b1, b2, b3] => ctr.count += u32::from_le_bytes([b0, b1, b2, b3]),
+        _ => return Err(ProgramError::InvalidArgument),
     };
 
-    acct_memo.serialize(&mut &mut account.data.borrow_mut()[..])?;
+    ctr.serialize(&mut &mut account.data.borrow_mut()[..])?;
 
     Ok(())
 }
